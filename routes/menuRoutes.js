0365068fbbc167ba288b4f4e9100d6e200bb9b7f
Andra Yusaldi Putra <andrayusaldi9@gmail.com>
2023-12-07 17:18:27 +0700

    perbaikan

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const menuController = require("../controllers/menuController");
+const menuController = require("../controller/menuController");
 
 router.get("/menus", menuController.getAllmenus);
 
